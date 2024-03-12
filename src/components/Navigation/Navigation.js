6b39571070515f94ed20196fa8350eb39dab8695
LucDictus <lucdictus161@gmail.com>
2024-03-12 08:59:32 +0100

    navigation without links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './Navigation.css';
 
 const Navigation = () => {
+ 
+
     return (
-        <div className='navigation-container'>
-            
-        </div>
+        <nav id="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                
+                <ul id="menu">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>Leaderboard</li></a>
+                    <a href="#"><li>How to play</li></a>
+                    <a href="#"><li>Account</li></a>
+                    <a href="#"><li>Log out</li></a>
+                </ul>
+            </div>
+
+            <div className="navigation-logo">
+                <img src="/images/Logo.png" alt="Logo" />
+            </div>
+        </nav> 
     );
 };
 
