60f29bd7d7cb22dcf22b021f32e9c09bf4384235
AlozieE0203 <40187949@roctilburg.nl>
2024-03-27 08:54:31 +0100

    leaderboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import LoginPage from './components/LoginPage/LoginPage.js';
+import Leaderboard from './components/leaderboard/LeaderBoard.js';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
+      <Leaderboard />
     </div>
   );
 }
